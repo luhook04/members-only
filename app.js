db51ac623896be2ee880dd6b8fefd7dc78c88057
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-07-06 21:50:54 -0400

    Install user authentication dependencies and set them up
    Install passport and express-session, and encode the session secret
    using dotenv.

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const passport = require("passport");
 
 var app = express();
 
@@ -22,6 +25,17 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(logger("dev"));
 app.use(express.json());
