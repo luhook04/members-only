2e8ceaa81360c985111a438b622b01cb37a1bd3f
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-07-06 21:05:02 -0400

    Create schema for userModel to store user data

---
+++ models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, maxLength: 15 },
+  password: { type: String, required: true, maxLength: 15 },
+  isMember: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
+  avatar: {
+    type: String,
+    required: true,
+    enum: ["alien1", "alien2", "alien3"],
+    default: "alien1",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
