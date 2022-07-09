dd74297942a4c3320bb4c5cd807037bad0b50aed
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-07-09 13:04:15 -0400

    Add create_message_post middleware function
    Allow users to create a message with message title and message text.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -7,3 +7,31 @@ exports.create_message_get = (req, res, next) => {
   }
   res.render("message_form", { title: "Create Message" });
 };
+
+exports.create_message_post = [
+  body("title").trim().isLength({ min: 1 }).escape(),
+  body("text").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("message_form", {
+        title: "Create Message",
+        errors: errors.array(),
+      });
+    }
+
+    const message = new Message({
+      user: req.user._id,
+      title: req.body.title,
+      text: req.body.text,
+      timestape: Date.now(),
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  },
+];
