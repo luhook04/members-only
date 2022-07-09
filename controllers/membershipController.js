3bbdf209f000f8d96b806445180b4f2eaba0cb4f
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-07-09 17:37:49 -0400

    Add admin.post middleware function in membershipController
    If user enters correct password, make the user an admin and redirect to
    the homepage.
    
    If user enters incorrect password, render member_form with the wrong
    password error.

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -52,3 +52,39 @@ exports.admin_get = (req, res, next) => {
     title: "Become an Admin",
   });
 };
+
+exports.admin_post = [
+  body("passcode")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("No password entered."),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    var user = new User(res.locals.currentUser);
+
+    if (!errors.isEmpty()) {
+      res.render("member_form", {
+        title: "Become an Admin",
+        errors: errors.array(),
+      });
+    } else if (req.body.passcode !== "adminhi") {
+      res.render("member_form", {
+        title: "Become an Admin",
+        error: "Wrong Password",
+      });
+    } else {
+      user.isAdmin = true;
+      User.findByIdAndUpdate(
+        res.locals.currentUser._id,
+        user,
+        {},
+        (err) => {
+          if (err) return next(err);
+          return res.redirect("/");
+        }
+      );
+    }
+  },
+];
